2666c2ec6abc59156a03a5aaaeaadce2ae49b7ad
illner <illner3@gmail.com>
2025-10-20 13:42:55 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Exit immediately if any command fails
+set -e
+
+# Remove the build directory if it exists
 if [ -d build ]; then rm -rf build; fi
 
 mkdir build
@@ -6,15 +12,15 @@ cd build
 # Release
 mkdir Release
 cd Release
-cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release ../..
-make
+cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release "../.."
+make -j8
 
 cd ..
 
 # Debug
 mkdir Debug
 cd Debug
-cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug ../..
-make
+cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug "../.."
+make -j8
 
 cd ..
